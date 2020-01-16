293ecf24b84cd1ea6bc51b6142733c40981886cd
ryandiebeste <44663422+ryandiebeste@users.noreply.github.com>
2020-01-16 11:55:15 +0200

    First commit

---
+++ bitcoin_reader/bitcoin_reader/Rest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Web;
+
+class Rest
+{
+    private static string API_KEY = "e24be655-2753-449c-ac83-b573fcf72407";
+
+    public  string GetBTC()
+    {
+        try
+        {
+           String val = makeAPICall();
+            return val;
+        }
+        catch (WebException e)
+        {
+            Console.WriteLine(e.Message);
+            return null; 
+         }
+    }
+
+     static string makeAPICall()
+    {
+        var URL = new UriBuilder("https://pro-api.coinmarketcap.com/v1/cryptocurrency/map");
+
+        var queryString = HttpUtility.ParseQueryString(string.Empty);
+        queryString["symbol"] = "BTC,USDT,BNB";
+
+        URL.Query = queryString.ToString();
+
+        var client = new WebClient();
+         client.Headers.Add("X-CMC_PRO_API_KEY", API_KEY);
+         client.Headers.Add("Accepts", "application/json");
+        return client.DownloadString(URL.ToString());
+        
+    }
+
+}
